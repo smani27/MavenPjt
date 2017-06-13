db4fcd38419e03d4250b8201f91c1f33a2aa73d2
manimarans <smani27@yahoo.co.in>
2017-06-13 15:46:40 +0530

    Changed the test method name from f to invoke browser and calling java method

--- firstPjt/src/test/java/com/test/TestNew.java
+++ firstPjt/src/test/java/com/test/TestNew.java
@@ -9,20 +9,21 @@ public class TestNew {
 	
 	New2Test nw;
   @Test
-  public void f() throws InterruptedException {
+  public void invokeBrowser() throws InterruptedException {
 	  System.out.println("This is my First Maven Test");
 	  
 	  System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 	  WebDriver driver = new ChromeDriver();
 	  driver.get("https://www.google.com");
 	  Thread.sleep(5000);
+	  driver.quit();
 	  System.out.println("Bamboo...");
 	  
-	 /* driver.quit();*/
+	  
   }
   
   @Test
-  public void f2(){
+  public void callingJavaMethod(){
 	  
 	  nw = new New2Test();
 	  nw.main(null);
